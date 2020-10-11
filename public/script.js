62c62515fecaaa4fb4a4b5460acc33ab118d8f7d
Shekhar Tyagi <notshekhar@gmail.com>
2020-10-11 20:04:14 +0530

    new

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
 let create_room_btn = document.querySelector(".create_new_room")
+let join_room_inp = document.querySelector(".join")
+let join_room_btn = document.querySelector(".join_room")
+
+join_room_btn.onclick = () => {
+    let room_id = join_room_inp.value
+    if (room_id.length > 0) {
+        location.href = `/${room_id}`
+    }
+}
 
 create_room_btn.onclick = function () {
     let room_id = v4()
